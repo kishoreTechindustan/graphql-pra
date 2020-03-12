c9a0aba668d144ba5233e80f6fedaf3f41d19872
kishore <kishore@techindustan.com>
2020-03-12 07:06:28 +0530

    mutations apply

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+app.use('/graphql',expressGraphQL({
+    schema:schema,
+    graphiql:true
+}))
+
+app.listen(5000,()=>{
+    console.log('Listening port 5000')
+})
+ 
